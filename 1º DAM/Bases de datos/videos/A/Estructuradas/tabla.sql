733d2bf6cadde88c15d165e9592987be429b8d13
AgusMADEV <agustin.morcillo1912@gmail.com>
2024-12-08 19:10:20 +0100

    1

---
+++ 1º DAM/Bases de datos/videos/A/Estructuradas/tabla.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `bandainamco`.`clientes`
+ (
+    `Identificador` INT(255) NOT NULL AUTO_INCREMENT , 
+    `nombre` VARCHAR(255) NOT NULL ,
+    `apellidos` VARCHAR(255) NOT NULL ,
+    `telefono` VARCHAR(255) NOT NULL ,
+    `email` VARCHAR(255) NOT NULL 
+    , PRIMARY KEY (`Identificador`)
+) ENGINE = InnoDB;
