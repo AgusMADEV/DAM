cd3095008236ae1d6be33768b65581e024321c32
AgusMADEV <agustin.morcillo1912@gmail.com>
2025-01-16 19:00:21 +0100

    SEO

---
+++ 1º DAM/Lenguajes de marcas y sistemas de gestión de información/003-/proyectos/Proyecto Apple (Mi)/065-Un poco de SEO/front/modulos/cabecera/cabecera.php
@@ -0,0 +1,52 @@
+<style>
+<?php include "cabecera.css" ?>
+</style>
+<header>
+    <div class="contenedor">
+        <img src="img/logoe2.svg">
+        <nav>
+            <ul>
+                <template id="elementomenu">
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </template>
+            </ul>
+        </nav>
+        <div id="supermenu">
+            <div class="columna">
+                <h3>Cabecera</h3>
+                <ul>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                </ul>
+            </div>
+            <div class="columna">
+                <h3>Cabecera</h3>
+                <ul>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                </ul>
+            </div>
+            <div class="columna">
+                <h3>Cabecera</h3>
+                <ul>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                    <li>Elemento</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+<script>
+<?php include "cabecera.js" ?>
+</script>
