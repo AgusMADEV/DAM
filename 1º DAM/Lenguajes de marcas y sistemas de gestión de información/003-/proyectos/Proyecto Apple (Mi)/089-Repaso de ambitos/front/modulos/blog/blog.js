f91b8c0a12876bc2238fb2123e5406ba473cc3f8
AgusMADEV <agustin.morcillo1912@gmail.com>
2025-01-23 19:49:56 +0100

    1

---
+++ 1º DAM/Lenguajes de marcas y sistemas de gestión de información/003-/proyectos/Proyecto Apple (Mi)/089-Repaso de ambitos/front/modulos/blog/blog.js
@@ -0,0 +1,19 @@
+function cargaBlog(){
+    // Carga elementos en el menú principal con las categorías
+    fetch("../back/?tabla=blog")                                                                                                        // Cargo un endpoint en el back
+    .then(function(response) {                                                                                                                      // Cuando obtenga respuesta
+        return response.json();                                                                                                                         // La convierto en json
+    })
+    .then(function(datos) {                                                                                                                             // Y cuando reciba datos
+        console.log(datos);
+        let principal = document.querySelector("main")
+        let plantilla = document.querySelector("#plantillaentrada")
+        datos.forEach(function(dato){
+            let instancia = plantilla.content.cloneNode(true);
+            instancia.querySelector("h4").textContent = dato.titulo
+            instancia.querySelector("time").textContent = dato.fecha
+            principal.appendChild(instancia)
+        })
+    })
+}
+cargaBlog();
---
+++ 1º DAM/Lenguajes de marcas y sistemas de gestión de información/003-/proyectos/Proyecto Apple (Mi)/091-Cargo un articulo del blog/front/modulos/heroes/heroes.php
@@ -0,0 +1,16 @@
+<style>
+    <?php include "heroes.css" ?>
+</style>
+<section id="heroes">
+    <template id="plantillaheroe">
+        <article class="heroe">
+            <h3>Nombre del Producto</h3>
+            <h4>Frase de Marketing</h4>
+            <button>Call to action 1</button>
+            <button>Call to action 2</button>
+        </article>
+    </template>
+</section>
+<script>
+    <?php include "heroes.js" ?>
+</script>
